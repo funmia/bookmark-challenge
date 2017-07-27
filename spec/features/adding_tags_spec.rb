8863e15100c0c532dd253e020f2a457e655f1a82
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-07-27 15:14:02 +0100

    Adding multiple tags

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,13 +1,22 @@
 feature 'Adding tags' do
 
-  scenario 'I can tag a single link' do
+  before(:each) do
     visit('/links/new')
     fill_in('title', with: 'Twitter')
     fill_in('url', with: 'https://twitter.com')
-    fill_in('tags', with: 'social')
+  end
 
+  scenario 'I can tag a single link' do
+    fill_in('tags', with: 'social')
     click_button("Add")
     link = Link.first
-    expect(link.tags.map(&:name)).to include('social')
+    expect(link.tags.map(&:name).join).to eq ('social')
+  end
+
+  scenario 'I can add multiple tags to a link' do
+    fill_in('tags', with: 'news, comedy' )
+    click_button("Add")
+    link = Link.first
+    expect(link.tags.map(&:name).join(", ")).to eq ('news, comedy')
   end
 end
